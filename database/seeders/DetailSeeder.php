887eb5f82f19a793b820c70f05cfafdcfb0020c2
hoge <130904564+Promochi0141@users.noreply.github.com>
2024-09-23 15:44:38 +0900

    日付の設定、テストデータの編集

--- database/seeders/DetailSeeder.php
+++ database/seeders/DetailSeeder.php
@@ -19,5 +19,20 @@ class DetailSeeder extends Seeder
             'url' => 'test_url',
             'place' => 'test_place',
         ]);
+        $detail2 = Detail::create([
+            'name' => 'test_name2',
+            'url' => 'test_url2',
+            'place' => 'test_place2',
+        ]);
+        $detail3 = Detail::create([
+            'name' => 'test_name3',
+            'url' => 'test_url3',
+            'place' => 'test_place3',
+        ]);
+        $detail4 = Detail::create([
+            'name' => 'test_name4',
+            'url' => 'test_url4',
+            'place' => 'test_place4',
+        ]);
     }
 }
--- database/seeders/ShiftSeeder.php
+++ database/seeders/ShiftSeeder.php
@@ -14,11 +14,33 @@ class ShiftSeeder extends Seeder
      */
     public function run(): void
     {
-        $shift = Shift::create([
+        $shift1 = Shift::create([
             'student_id' => '1',
             'details_id' => 1,
+            'date' => '2024-01-01',
             'start_time' => '09:00:00',
+            'end_time' => '12:00:00',
+        ]);
+        $shift2 = Shift::create([
+            'student_id' => '1',
+            'details_id' => 2,
+            'date' => '2024-01-01',
+            'start_time' => '12:00:00',
             'end_time' => '17:00:00',
+        ]);
+        $shift3 = Shift::create([
+            'student_id' => '1',
+            'details_id' => 3,
+            'date' => '2024-01-01',
+            'start_time' => '17:00:00',
+            'end_time' => '24:00:00',
+        ]);
+        $shift4 = Shift::create([
+            'student_id' => '1',
+            'details_id' => 4,
+            'date' => '2024-01-01',
+            'start_time' => '01:00:00',
+            'end_time' => '09:00:00',
         ]);
     }
 }
